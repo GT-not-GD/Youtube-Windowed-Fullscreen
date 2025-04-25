d8c90404540d484a48d1f0fa3e1e423216218e58
GT <126884637+GT-not-GD@users.noreply.github.com>
2025-04-26 01:36:55 +0800

    Update Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js

--- Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js
+++ Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js
@@ -7,7 +7,8 @@
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
-// @license      MIT https://raw.githubusercontent.com/GT-not-GD/youtube-windowed-fullscreen-script/main/Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js // 
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/GT-not-GD/youtube-windowed-fullscreen-script/main/Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js // 
 // @updateURL    https://raw.githubusercontent.com/GT-not-GD/youtube-windowed-fullscreen-script/main/Youtube-Auto-Theater-Mode-Windowed-Fullscreen-1.0.user.js // 
 // ==/UserScript==
 
